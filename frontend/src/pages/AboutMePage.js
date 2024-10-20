ac046e1dd0bcd9c2d0d4d0f4b79090cb6220200b
Rahulk-dot <76915568+Rahulk-dot@users.noreply.github.com>
2024-10-20 08:16:27 +0530

    formatter changed to prettier (#2)

--- frontend/src/pages/AboutMePage.js
+++ frontend/src/pages/AboutMePage.js
@@ -1,15 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const AboutMePage = () => {
-
-    return ( <
-        div className = "h-screen w-full bg-black text-white flex overflow-x-auto justify-center font-customElements text-8xl p-5" >
-        <
-        h1 > About Me < /h1>
-
-        <
-        /div>
-    );
+  return (
+    <div className="h-screen w-full bg-black text-white flex overflow-x-auto justify-center font-customElements text-8xl p-5">
+      <h1> About Me </h1>
+    </div>
+  );
 };
 
 export default AboutMePage;
--- frontend/src/pages/EducationPage.js
+++ frontend/src/pages/EducationPage.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const EducationPage = () => {
-
-    return ( <
-        div className = "h-screen w-full bg-black text-white flex overflow-x-auto justify-center font-customElements text-8xl p-5" >
-        <
-        h1 > Education < /h1> <
-        /div>
-    );
+  return (
+    <div className="h-screen w-full bg-black text-white flex overflow-x-auto justify-center font-customElements text-8xl p-5">
+      <h1> Education </h1>{" "}
+    </div>
+  );
 };
 
 export default EducationPage;
