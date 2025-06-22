85995d9ca045c7094dc4e9b1501c803281b45b88
Rahulk-dot <rahul.kaushik0719@gmail.com>
2025-06-22 20:40:13 +0530

    responsivess fixed for menu page and some code formating

--- frontend/src/pages/MenuPage.js
+++ frontend/src/pages/MenuPage.js
@@ -4,52 +4,27 @@ import { useNavigate } from 'react-router-dom';
 const MenuPage = () => {
   const navigate = useNavigate();
 
-  const handleEducationClick = () => {
-    navigate('/education');
-  };
-
-  const handleProjectClick = () => {
-    navigate('/project-menu');
-  };
-
-  const handleCoCurricularClick = () => {
-    navigate('/co-curricular');
-  };
-
-  const handleAboutMeClick = () => {
-    navigate('/about-me');
-  };
-
-  const handleCareerClick = () => {
-    navigate('/career');
-  };
+  const links = [
+    { text: 'Education', onClick: () => navigate('/education') },
+    { text: 'Projects', onClick: () => navigate('/project-menu') },
+    { text: 'Co - Curricular', onClick: () => navigate('/co-curricular') },
+    { text: 'About Me', onClick: () => navigate('/about-me') },
+    { text: 'Career', onClick: () => navigate('/career') },
+  ];
 
   return (
-    <div className="h-screen w-full bg-black text-white flex overflow-x-auto items-center font-customElements p-20">
-      <ul className="list-none">
-        <li
-          className="relative text-8xl p-5 cursor-pointer"
-          onClick={handleEducationClick}
-        >
-          <span className="absolute -left-10 top-1/2 transform -translate-y-1/2 w-4 h-4 bg-white rotate-45 transition-transform duration-300 ease-in-out hover:rotate-90"></span>
-          Education
-        </li>
-        <li className="relative text-8xl p-5 cursor-pointer" onClick={handleProjectClick}>
-          <span className="absolute -left-10 top-1/2 transform -translate-y-1/2 w-4 h-4 bg-white rotate-45 transition-transform duration-300 ease-in-out hover:rotate-90"></span>
-          Projects
-        </li>
-        <li className="relative text-8xl p-5 cursor-pointer" onClick={handleCoCurricularClick}>
-          <span className="absolute -left-10 top-1/2 transform -translate-y-1/2 w-4 h-4 bg-white rotate-45 transition-transform duration-300 ease-in-out hover:rotate-90"></span>
-          Co - Curricular
-        </li>
-        <li className="relative text-8xl p-5 cursor-pointer" onClick={handleAboutMeClick}>
-          <span className="absolute -left-10 top-1/2 transform -translate-y-1/2 w-4 h-4 bg-white rotate-45 transition-transform duration-300 ease-in-out hover:rotate-90"></span>
-          About Me
-        </li>
-        <li className="relative text-8xl p-5 cursor-pointer" onClick={handleCareerClick}>
-          <span className="absolute -left-10 top-1/2 transform -translate-y-1/2 w-4 h-4 bg-white rotate-45 transition-transform duration-300 ease-in-out hover:rotate-90"></span>
-          Career
-        </li>
+    <div className="min-h-screen w-full bg-black text-white font-customElements px-4 sm:px-10 md:px-16 lg:px-20 py-20">
+      <ul className="list-none w-full space-y-6 sm:space-y-8 md:space-y-10 lg:space-y-12">
+        {links.map((item, index) => (
+          <li
+            key={index}
+            className="relative text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl py-3 px-4 cursor-pointer transition duration-300 ease-in-out"
+            onClick={item.onClick}
+          >
+            <span className="absolute -left-6 sm:-left-8 top-1/2 transform -translate-y-1/2 w-3 sm:w-4 h-3 sm:h-4 bg-white rotate-45 hover:rotate-90 transition-transform duration-300 ease-in-out"></span>
+            {item.text}
+          </li>
+        ))}
       </ul>
     </div>
   );
